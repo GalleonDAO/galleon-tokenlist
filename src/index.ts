27d4e22b6fcd63d5e5051175011e7930b7d7fc45
Andrew <andywilkinson1993@gmail.com>
2022-04-09 15:35:02 +0100

    tokenlists updated

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@
  * can be imported from the published npm package like so:
  *
  * ```js
- * import PackageName from '@galleondao/setswap-tokenlist';
+ * import PackageName from '@indexcoop/tokenlists';
  * ```
  *
  * And non-default exports can be imported like this:
  *
  * ```jss
- * import {functionName} from '@galleondao/setswap-tokenlist';
+ * import {functionName} from '@indexcoop/tokenlists';
  * ```
  */
 
@@ -28,14 +28,13 @@ export interface TokenData {
   logoURI: string
 }
 
-export const SetSwapProductsArbitrum = SETSWAP_ARBITRUM_TOKENS
-export const SetSwapProductsAvalanche = SETSWAP_AVALANCHE_TOKENS
-export const SetSwapProductsMainnet = SETSWAP_MAINNET_TOKENS
-export const SetSwapProductsMatic = SETSWAP_MATIC_TOKENS
-export const SetSwapProductsOptimism = SETSWAP_OPTIMISM_TOKENS
-
-export const SetSwapArbitrumTokens = ARBITRUM_TOKENS
-export const SetSwapAvalancheTokens = AVALANCHE_TOKENS
-export const SetSwapMainnetTokens = MAINNET_TOKENS
-export const SetSwapMaticTokens = MATIC_TOKENS
-export const SetSwapOptimismTokens = OPTIMISM_TOKENS
+export const ArbitrumTokens = ARBITRUM_TOKENS
+export const SetSwapArbitrumTokens = SETSWAP_ARBITRUM_TOKENS
+export const AvalancheTokens = AVALANCHE_TOKENS
+export const SetSwapAvalancheTokens = SETSWAP_AVALANCHE_TOKENS
+export const MainnetTokens = MAINNET_TOKENS
+export const SetSwapMainnetTokens = SETSWAP_MAINNET_TOKENS
+export const MaticTokens = MATIC_TOKENS
+export const SetSwapMaticTokens = SETSWAP_MATIC_TOKENS
+export const OptimismTokens = OPTIMISM_TOKENS
+export const SetSwapOptimismTokens = SETSWAP_OPTIMISM_TOKENS
--- src/lists/avalanche.ts
+++ src/lists/avalanche.ts
@@ -1,8 +1,460 @@
-import { TokenData } from '..'
+import {TokenData} from '..';
 
-const SETSWAP_AVALANCHE_TOKENS: TokenData[] = []
+const SETSWAP_AVALANCHE_TOKENS: TokenData[] = [];
 
-const AVALANCHE_TOKENS: TokenData[] = [...SETSWAP_AVALANCHE_TOKENS]
+const AVALANCHE_TOKENS: TokenData[] = [
+  {
+    chainId: 43114,
+    address: '0x60781C2586D68229fde47564546784ab3fACA982',
+    decimals: 18,
+    name: 'Pangolin',
+    symbol: 'PNG',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/bridge-tokens/main/avalanche-tokens/0x60781C2586D68229fde47564546784ab3fACA982/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5',
+    decimals: 18,
+    name: 'BENQI',
+    symbol: 'QI',
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x264c1383EA520f73dd837F915ef3a732e204a493',
+    decimals: 18,
+    name: 'Binance',
+    symbol: 'BNB',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/bridge-tokens/main/avalanche-tokens/0x264c1383EA520f73dd837F915ef3a732e204a493/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xf20d962a6c8f70c731bd838a3a388D7d48fA6e15',
+    decimals: 18,
+    name: 'Ether',
+    symbol: 'ETH',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/bridge-tokens/main/avalanche-tokens/0xf20d962a6c8f70c731bd838a3a388D7d48fA6e15/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
+    decimals: 18,
+    name: 'Wrapped AVAX',
+    symbol: 'WAVAX',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/bridge-tokens/main/avalanche-tokens/0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xd501281565bf7789224523144Fe5D98e8B28f267',
+    decimals: 18,
+    name: '1INCH Token',
+    symbol: '1INCH.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/1INCH/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x63a72806098Bd3D9520cC43356dD78afe5D386D9',
+    decimals: 18,
+    name: 'Aave Token',
+    symbol: 'AAVE.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/AAVE/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x2147EFFF675e4A4eE1C2f918d181cDBd7a8E208f',
+    decimals: 18,
+    name: 'AlphaToken',
+    symbol: 'ALPHA.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/ALPHA/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x98443B96EA4b0858FDF3219Cd13e98C7A4690588',
+    decimals: 18,
+    name: 'Basic Attention Token',
+    symbol: 'BAT.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/BAT/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xc3048E19E76CB9a3Aa9d77D8C03c29Fc906e2437',
+    decimals: 18,
+    name: 'Compound',
+    symbol: 'COMP.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/COMP/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x249848BeCA43aC405b8102Ec90Dd5F22CA513c06',
+    decimals: 18,
+    name: 'Curve DAO Token',
+    symbol: 'CRV.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/CRV/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xd586E7F844cEa2F87f50152665BCbc2C279D8d70',
+    decimals: 18,
+    name: 'Dai Stablecoin',
+    symbol: 'DAI.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/DAI/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x8a0cAc13c7da965a312f08ea4229c37869e85cB9',
+    decimals: 18,
+    name: 'Graph Token',
+    symbol: 'GRT.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/GRT/logo.png',
+  },
+  {
+    address: '0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd',
+    chainId: 43114,
+    name: 'Joe Token',
+    symbol: 'JOE',
+    decimals: 18,
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x5947BB275c521040051D82396192181b413227A3',
+    decimals: 18,
+    name: 'ChainLink Token',
+    symbol: 'LINK.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/LINK/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x4C9B4E1AC6F24CdE3660D5E4Ef1eBF77C710C084',
+    decimals: 18,
+    name: 'Lydia Finance',
+    symbol: 'LYD',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/bridge-tokens/main/avalanche-tokens/0x4C9B4E1AC6F24CdE3660D5E4Ef1eBF77C710C084/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x88128fd4b259552A9A1D457f435a6527AAb72d42',
+    decimals: 18,
+    name: 'Maker',
+    symbol: 'MKR.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/MKR/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xe896CDeaAC9615145c0cA09C8Cd5C25bced6384c',
+    decimals: 18,
+    name: 'Penguin Finance',
+    symbol: 'PEFI',
+    logoURI:
+      'https://raw.githubusercontent.com/Penguin-Finance/png-files/main/pefiv2_250x250.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xE1463E8991c8A62e64b77b5Fb6B22F190344C2A9',
+    decimals: 18,
+    name: 'PolkastarterToken',
+    symbol: 'POLS',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/bridge-tokens/main/avalanche-tokens/0xE1463E8991c8A62e64b77b5Fb6B22F190344C2A9/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x02D980A0D7AF3fb7Cf7Df8cB35d9eDBCF355f665',
+    decimals: 18,
+    name: 'SHIBA INU',
+    symbol: 'SHIB.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/SHIB/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xBeC243C995409E6520D7C41E404da5dEba4b209B',
+    decimals: 18,
+    name: 'Synthetix Network Token',
+    symbol: 'SNX.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/SNX/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x37B608519F91f70F2EeB0e5Ed9AF4061722e4F76',
+    decimals: 18,
+    name: 'SushiToken',
+    symbol: 'SUSHI.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/SUSHI/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xc7B5D72C836e718cDA8888eaf03707fAef675079',
+    decimals: 18,
+    name: 'TrustSwap Token',
+    symbol: 'SWAP.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/SWAP/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x3Bd2B1c7ED8D396dbb98DED3aEbb41350a5b2339',
+    decimals: 18,
+    name: 'UMA Voting Token v1',
+    symbol: 'UMA.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/UMA/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x8eBAf22B6F053dFFeaf46f4Dd9eFA95D89ba8580',
+    decimals: 18,
+    name: 'Uniswap',
+    symbol: 'UNI.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/UNI/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664',
+    decimals: 6,
+    name: 'USD Coin',
+    symbol: 'USDC.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/USDC/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x1F1FE1eF06ab30a791d6357FdF0a7361B39b1537',
+    decimals: 9,
+    name: 'sled.finance',
+    symbol: 'SFI',
+    logoURI:
+      'https://raw.githubusercontent.com/sled-finance/media/main/sled_token_fl.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xC38f41A296A4493Ff429F1238e030924A1542e50',
+    decimals: 18,
+    name: 'Snowball',
+    symbol: 'SNOB',
+    logoURI:
+      'https://raw.githubusercontent.com/Snowball-Finance/Assets/main/Uphill%20snowball.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x390ba0fb0Bd3Aa2a5484001606329701148074e6',
+    decimals: 18,
+    name: 'THORChain ETH.RUNE',
+    symbol: 'RUNE',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/bridge-tokens/main/avalanche-tokens/0x390ba0fb0Bd3Aa2a5484001606329701148074e6/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x50b7545627a5162F82A992c33b87aDc75187B218',
+    decimals: 8,
+    name: 'Wrapped BTC',
+    symbol: 'WBTC.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/WBTC/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x49D5c2BdFfac6CE2BFdB6640F4F80f226bc10bAB',
+    decimals: 18,
+    name: 'Wrapped ETH',
+    symbol: 'WETH.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/WETH/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xaBC9547B534519fF73921b1FBA6E672b5f58D083',
+    decimals: 18,
+    name: 'Wootrade Network',
+    symbol: 'WOO.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/WOO/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x59414b3089ce2AF0010e7523Dea7E2b35d776ec7',
+    decimals: 18,
+    name: 'Yield Yak',
+    symbol: 'YAK',
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0x59414b3089ce2AF0010e7523Dea7E2b35d776ec7/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x9eAaC1B23d935365bD7b542Fe22cEEe2922f52dc',
+    decimals: 18,
+    name: 'yearn.finance',
+    symbol: 'YFI.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/YFI/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x596fA47043f99A4e0F122243B841E55375cdE0d2',
+    decimals: 18,
+    name: '0x Protocol Token',
+    symbol: 'ZRX.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/ZRX/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xc7198437980c041c805A1EDcbA50c1Ce5db95118',
+    decimals: 6,
+    name: 'Tether USD',
+    symbol: 'USDT.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/USDT/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x1C20E891Bab6b1727d14Da358FAe2984Ed9B59EB',
+    decimals: 18,
+    name: 'TrueUSD',
+    symbol: 'TUSD',
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0x1C20E891Bab6b1727d14Da358FAe2984Ed9B59EB/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x564A341Df6C126f90cf3ECB92120FD7190ACb401',
+    decimals: 6,
+    name: 'BiLira',
+    symbol: 'TRYB',
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0x564A341Df6C126f90cf3ECB92120FD7190ACb401/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x19860CCB0A68fd4213aB9D8266F7bBf05A8dDe98',
+    decimals: 18,
+    name: 'Binance USD',
+    symbol: 'BUSD.e',
+    logoURI:
+      'https://raw.githubusercontent.com/ava-labs/avalanche-bridge-resources/main/tokens/BUSD/logo.png',
+  },
+  {
+    address: '0x4fbf0429599460D327BD5F55625E30E4fC066095',
+    chainId: 43114,
+    name: 'TSD',
+    symbol: 'TSD',
+    decimals: 18,
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0x4fbf0429599460D327BD5F55625E30E4fC066095/logo.png',
+  },
+  {
+    address: '0x3509f19581aFEDEff07c53592bc0Ca84e4855475',
+    chainId: 43114,
+    name: 'xDollar Stablecoin',
+    symbol: 'xUSD',
+    decimals: 18,
+    logoURI:
+      'https://raw.githubusercontent.com/xDollar-Finance/logo/master/logo_256x256.png',
+  },
+  {
+    address: '0x130966628846BFd36ff31a822705796e8cb8C18D',
+    chainId: 43114,
+    name: 'Magic Internet Money',
+    symbol: 'MIM',
+    decimals: 18,
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0x130966628846BFd36ff31a822705796e8cb8C18D/logo.png',
+  },
+  {
+    address: '0x783C08b5F26E3daf8C4681F3bf49844e425b6393',
+    chainId: 43114,
+    name: 'Avaware USD',
+    symbol: 'AUSD',
+    decimals: 18,
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0x783C08b5F26E3daf8C4681F3bf49844e425b6393/logo.png',
+  },
+  {
+    address: '0x346A59146b9b4a77100D369a3d18E8007A9F46a6',
+    chainId: 43114,
+    name: 'AVAI',
+    symbol: 'AVAI',
+    decimals: 18,
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0x346A59146b9b4a77100D369a3d18E8007A9F46a6/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xD24C2Ad096400B6FBcd2ad8B24E7acBc21A1da64',
+    decimals: 18,
+    name: 'Frax',
+    symbol: 'FRAX',
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0xD24C2Ad096400B6FBcd2ad8B24E7acBc21A1da64/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0xAafd2577Fb67366d3C89DB0d627C49D769ee2e5D',
+    decimals: 18,
+    name: 'Stabilize USD',
+    symbol: 'SUSD',
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0xAafd2577Fb67366d3C89DB0d627C49D769ee2e5D/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x5c49b268c9841AFF1Cc3B0a418ff5c3442eE3F3b',
+    decimals: 18,
+    name: 'Mai',
+    symbol: 'MIMATIC',
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0x5c49b268c9841AFF1Cc3B0a418ff5c3442eE3F3b/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x026187BdbC6b751003517bcb30Ac7817D5B766f8',
+    decimals: 18,
+    name: 'Defrost Finance H2O',
+    symbol: 'H2O',
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0x026187BdbC6b751003517bcb30Ac7817D5B766f8/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x6feFd97F328342a8A840546A55FDcfEe7542F9A8',
+    decimals: 18,
+    name: 'agEUR',
+    symbol: 'AGEUR',
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0x6feFd97F328342a8A840546A55FDcfEe7542F9A8/logo.png',
+  },
+  {
+    chainId: 43114,
+    address: '0x260Bbf5698121EB85e7a74f2E45E16Ce762EbE11',
+    decimals: 6,
+    name: 'Axelar Wrapped UST',
+    symbol: 'UST',
+    logoURI:
+      'https://raw.githubusercontent.com/pangolindex/tokens/main/assets/0x260Bbf5698121EB85e7a74f2E45E16Ce762EbE11/logo.png',
+  },
+  ...SETSWAP_AVALANCHE_TOKENS,
+];
 
-export { SETSWAP_AVALANCHE_TOKENS }
-export default AVALANCHE_TOKENS
+export {SETSWAP_AVALANCHE_TOKENS};
+export default AVALANCHE_TOKENS;
