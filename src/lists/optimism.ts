7e20de07c75405adb6e038cdb876de0867407b29
Davy Jones <hello@galleon.community>
2022-06-14 15:03:29 +0100

    added bye, removed soluvax

--- src/lists/optimism.ts
+++ src/lists/optimism.ts
@@ -2,40 +2,13 @@ import { TokenData } from '..'
 
 const GALLEON_OPTIMISM_TOKENS: TokenData[] = [
   {
-    name: 'SOLUNAVAX Index',
-    address: '0xbA6a2Fa321BB06D668c5192Be77428106c5C01E5',
-    symbol: 'SOLUNAVAX',
+    name: 'Basis Yield ETH Index',
+    address: '0x927Eb0dBC5c3FD172Fdfa72D563f71612eCB6122',
+    symbol: 'BYE',
     decimals: 18,
     chainId: 10,
     logoURI:
-      'https://github.com/GalleonDAO/galleon-tokenlist/blob/main/logos/solunavax-200px.png?raw=true',
-  },
-  {
-    name: 'SOL 1X',
-    address: '0x95fffb13856d2be739a862f9b645573e5c838bdd',
-    symbol: 'SOL1X',
-    decimals: 18,
-    chainId: 10,
-    logoURI:
-      'https://assets.coingecko.com/coins/images/23503/small/sol1x.png?1644296700',
-  },
-  {
-    name: 'AVAX 1X',
-    address: '0x522439fb1da6db24f18baab1782486b55fe3a7b6',
-    symbol: 'AVAX1X',
-    decimals: 18,
-    chainId: 10,
-    logoURI:
-      'https://assets.coingecko.com/coins/images/23504/small/avax1x.png?1644296887',
-  },
-  {
-    name: 'LUNA 1X',
-    address: '0x19f0622903a977a24bb47521732e6291002a4ede',
-    symbol: 'LUNA1X',
-    decimals: 18,
-    chainId: 10,
-    logoURI:
-      'https://assets.coingecko.com/coins/images/23506/small/luna1x.png?1644297421',
+      'https://github.com/GalleonDAO/galleon-tokenlist/blob/main/logos/BYE.png?raw=true',
   },
 ]
 
